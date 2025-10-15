8ed2136629e6c9b0ae9fceb048f72b930fe97f1a
ArchSotelo <i202015028@cibertec.edu.pe>
2025-10-14 21:21:44 -0500

    Proyecto iniciado

---
+++ src/main/java/com/T2/LP2_T2_SOTELO_CARLOS/Lp2T2SoteloCarlosApplication.java
@@ -0,0 +1,13 @@
+package com.T2.LP2_T2_SOTELO_CARLOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2SoteloCarlosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2SoteloCarlosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/T2/LP2_T2_SOTELO_CARLOS/Lp2T2SoteloCarlosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.T2.LP2_T2_SOTELO_CARLOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2T2SoteloCarlosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
